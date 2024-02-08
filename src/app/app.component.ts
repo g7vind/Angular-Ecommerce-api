186b614d4bcccf94ffaa3ccf6bd1de2c7a35293a
g7vind <govindmh003@gmail.com>
2024-02-08 17:03:52 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ApiService } from './api.service';
+import { SlicePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SlicePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(public api: ApiService) { }
   title = 'Ecart';
+  data:any;
+  ngOnInit() {  
+    this.api.getData().subscribe(response=>{
+      this.data = response;
+    }); 
+  }
 }
